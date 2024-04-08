7248e6cf2c70c8449a5b483cee155174d2b60297
jules12345sqdd <164395593+jules12345sqdd@users.noreply.github.com>
2024-04-08 18:44:51 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["{/} Collège Saint-Martin"];
 
 
 let currentIndex = 0;
